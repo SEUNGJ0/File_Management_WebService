b10a7124566c7715e8faa98ae1eb8071c53f6a93
SEUNGJ0 <kuksj0312@naver.com>
2023-12-31 00:07:31 +0900

    DB를 MySQL로 변경
    기존의 SQLite3 에서 MySQL로 변경.
    데이터도 이동 완료

--- config/settings.py
+++ config/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Board_Website_Project_DB',
+        'USER': 'ksj',
+        'PASSWORD': get_secret('MySQL_Password'),
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
